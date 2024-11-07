2d5740eabe3061c1d4fce98d791f00f7cb7785e6
Alexander Ostrovsky <a-ostrovsky@users.noreply.github.com>
2024-11-07 20:39:54 +0100

    removed unused dependencies

--- src/CppPad.Gui/ViewModels/ViewModelBase.cs
+++ src/CppPad.Gui/ViewModels/ViewModelBase.cs
@@ -1,5 +1,34 @@
-using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace CppPad.Gui.ViewModels;
 
-public class ViewModelBase : ObservableObject;
+public class ViewModelBase : INotifyPropertyChanged, INotifyPropertyChanging
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+    public event PropertyChangingEventHandler? PropertyChanging;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected virtual void OnPropertyChanging([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(propertyName));
+    }
+
+    protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value))
+        {
+            return false;
+        }
+
+        OnPropertyChanging(propertyName);
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+}
