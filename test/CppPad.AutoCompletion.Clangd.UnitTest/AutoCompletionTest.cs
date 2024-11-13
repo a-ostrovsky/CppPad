97163ee19111f73029f18a9506c6043403a5828e
Alexander Ostrovsky <a-ostrovsky@users.noreply.github.com>
2024-11-13 22:52:14 +0100

    Added more test for auto completion

--- test/CppPad.AutoCompletion.Clangd.UnitTest/AutoCompletionTest.cs
+++ test/CppPad.AutoCompletion.Clangd.UnitTest/AutoCompletionTest.cs
@@ -4,6 +4,7 @@ using CppPad.AutoCompletion.Clangd.Impl;
 using CppPad.AutoCompletion.Clangd.UnitTest.Mocks;
 using CppPad.AutoCompletion.Interface;
 using CppPad.Common;
+using CppPad.CompilerAdapter.Interface;
 using CppPad.ScriptFile.Interface;
 using Microsoft.Extensions.Logging.Abstractions;
 
@@ -32,6 +33,30 @@ public class InitializationTest : IDisposable
         _clangdService.Dispose();
         _clangdProcessProxyMock.Kill();
         GC.SuppressFinalize(this);
+    }
+    
+    [Fact]
+    public async Task OpenFileAsync_ShouldSendDidOpenNotification()
+    {
+        // Arrange
+        var scriptDocument = new ScriptDocument
+        {
+            FileName = "test.cpp",
+            Identifier = new Identifier("test.cpp"),
+            Script = new Script
+            {
+                Content = "int main() { return 0; }",
+                CppStandard = CppStandard.Cpp17
+            }
+        };
+
+        // Act
+        await _clangdService.OpenFileAsync(scriptDocument);
+
+        // Assert
+        var sentMessages = _clangdProcessProxyMock.GetJsonObjects();
+        Assert.Contains(sentMessages, message =>
+            message["method"]?.GetValue<string>() == "textDocument/didOpen");
     }
 
     [Fact]
@@ -67,4 +92,31 @@ public class InitializationTest : IDisposable
         Assert.NotEmpty(completionList);
         Assert.Contains(completionList, item => item.Label == "std::cout");
     }
+    
+    [Fact]
+    public async Task CloseFileAsync_ShouldSendDidCloseNotification()
+    {
+        // Arrange
+        var scriptDocument = new ScriptDocument
+        {
+            FileName = "test.cpp",
+            Identifier = new Identifier("test.cpp"),
+            Script = new Script
+            {
+                Content = "int main() { return 0; }",
+                CppStandard = CppStandard.Cpp17
+            }
+        };
+
+        await _clangdService.OpenFileAsync(scriptDocument);
+
+        // Act
+        await _clangdService.CloseFileAsync(scriptDocument);
+
+        // Assert
+        var sentMessages = _clangdProcessProxyMock.GetJsonObjects();
+        Assert.Contains(sentMessages, message =>
+            message["method"]?.GetValue<string>() == "textDocument/didClose");
+    }
+
 }
